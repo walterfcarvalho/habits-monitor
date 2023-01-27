9efac4f4b2533550988f7a3a17e1a3ce86751981
Valter f Carvalho <walterfcarvalho@gmail.com>
2023-01-27 14:31:01 +0000

    gjhghgjhg

--- src/Firebase/config.js
+++ src/Firebase/config.js
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   apiKey: "AIzaSyDr5n-LKnmavyGosqK9FjJadkHM4CAG9Og",
-  //authDomain: "hmonitor-57097.firebaseapp.com",
-  authDomain: "habits-monitor.vercel.app/",
+  authDomain: "hmonitor-57097.firebaseapp.com",
+  //authDomain: "habits-monitor.vercel.app/",
   projectId: "hmonitor-57097",
   storageBucket: "hmonitor-57097.appspot.com",
   messagingSenderId: "1038874301708",
